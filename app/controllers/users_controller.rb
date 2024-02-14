f36ca9b9739a09341993e5d7ad4195dc57993689
Annie Huynh <150502695+ahuynh3a@users.noreply.github.com>
2024-02-14 22:58:50 +0000

    added photos relations and nav bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,15 @@ class UsersController < ApplicationController
 
     render({:template => "user_templates/index"})
   end 
+
+  def show
+    #Parameters: {"path_username"=>"anisa"}
+    url_username = params.fetch("path_username")
+
+    matching_usernames = User.where({:username => url_username})
+
+    @the_user = matching_usernames.first
+
+    render({:template=>"user_templates/show"})
+  end
 end
